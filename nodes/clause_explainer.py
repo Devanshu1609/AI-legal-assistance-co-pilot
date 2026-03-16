0b05cd40a782fa1d1260c911ec9eabc72e4d7ef3
Devanshu1609 <devanshukumarsingh03@gmail.com>
2026-03-16 11:13:37 +0530

    clause explainer node

---
+++ nodes/clause_explainer.py
@@ -0,0 +1,37 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal
+from models.chat_model import llm_with_tools, chat_model
+from state.agent_state import AgentState
+from config import CLAUSE_EXPLAINER_PATH
+
+def explain_clause(preferred_mode: Literal["chat_model", "tools"] = "chat_model"):
+
+    def clause_explainer(state: AgentState) -> AgentState:
+        try:
+            template = open(CLAUSE_EXPLAINER_PATH).read()
+        except FileNotFoundError:
+            raise ValueError(f"Prompt file missing at {CLAUSE_EXPLAINER_PATH}")
+
+        prompt_template = PromptTemplate(
+            input_variables=["extracted_text", "summary"],
+            template=template,
+        )
+
+        # IMPORTANT: always get a usable final text
+        # Tools can run, but final result must always include market_analysis
+        chain = prompt_template | chat_model
+
+        print("Invoking Clause Explainer...")
+        response = chain.invoke({"extracted_text": state["extracted_text"], "summary": state["summary"]})
+        print("Clause Explainer completed.")
+        print(f"Clause Explainer Response: {response.content}")
+
+        return {
+            "clause_explanation": response.content,
+            "messages": [HumanMessage(state["extracted_text"]), response],
+        }
+
+    return clause_explainer
