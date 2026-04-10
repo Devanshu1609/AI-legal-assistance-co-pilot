4663ad811afb2e2d35e1cf9affaf0908010cc3a9
Devanshu1609 <devanshukumarsingh03@gmail.com>
2026-04-11 00:12:58 +0530

    changes genai configuration

--- models/chat_model.py
+++ models/chat_model.py
@@ -1,10 +1,11 @@
 import os
 from dotenv import load_dotenv
-from google.generativeai import configure, ChatSession
+# from google.generativeai import configure, ChatSession
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chat_models import init_chat_model
 from tools.web_search_tool import web_search
 from config import TEMPERATURE, MAX_NEW_TOKENS
+import google.generativeai as genai
 
 load_dotenv()
 
@@ -13,7 +14,8 @@ if not api_key:
     raise ValueError("GOOGLE_API_KEY environment variable is not set.")
 
 # Configure Gemini API key
-configure(api_key=api_key)
+genai.configure(api_key="YOUR_API_KEY")
+
 
 # Use Gemini model
 # chat_model = ChatGoogleGenerativeAI(
